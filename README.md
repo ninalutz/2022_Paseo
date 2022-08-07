dbf788b231202484172fd1ce5b2817680617f5f5
ninalutz <nlutz@mit.edu>
2022-08-07 11:39:41 -0700

    make code cleaner

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 2022_Paseo
+
+p5_face_2 = debug view of the real thing, no recording
+p5_face = very raw view 
+face_vid_audio = saves audio and video
--- face_vid_audio/sketch.js
+++ face_vid_audio/sketch.js
@@ -1,19 +1,5 @@
-// A choice for number of keypoints: 7,33,68,468
-
-// // === bare minimum 7 points ===
-// var VTX = VTX7;
-
-// === important facial feature 33 points ===
-// var VTX = VTX33;
-
-// === standard facial landmark 68 points ===
-// var VTX = VTX68;
-
-// === full facemesh 468 points ===
-// var VTX = VTX468;
-
-//do the mouth only
-var VTX = mouth_landmarks;
+//Config for video
+var VTX = outer_mouth_landmarks;
 
 var facemeshModel = null; // this will be loaded with the facemesh model
                           // WARNING: do NOT call it 'model', because p5 already has something called 'model'
@@ -24,13 +10,126 @@ var statusText = "Loading facemesh model...";
 
 var myFaces = []; // faces detected in this browser
                   // currently facemesh only supports single face, so this will be either empty or singleton
-
 var capture; // webcam capture, managed by p5.js
 
+// Load the MediaPipe facemesh model assets.
+facemesh.load().then(function(_model){
+  statusText = "Model loaded."
+  facemeshModel = _model;
+})
 
+//Config for recording
 var btn = document.querySelector('button');
 var  chunks = [];
 let mic, soundRecorder, soundFile;
+
+
+function setup() {
+  configSound();
+  btn = document.querySelector('button');
+  btn.onclick = recordVideo;
+
+  createCanvas(1200, 720);
+  capture = createCapture(VIDEO);
+  
+  // capture = createVideo("test.mov");
+
+  // this is to make sure the capture is loaded before asking facemesh to take a look
+  // otherwise facemesh will be very unhappy
+  capture.elt.onloadeddata = function(){
+    console.log("video initialized");
+    videoDataLoaded = true;
+  }
+  
+  capture.hide();
+  background(0)
+}
+
+
+function draw() {
+  strokeJoin(ROUND); //otherwise super gnarly
+  
+  if (facemeshModel && videoDataLoaded){ // model and video both loaded, 
+    
+    facemeshModel.estimateFaces(capture.elt).then(function(_faces){
+      // we're faceling an async promise
+      // best to avoid drawing something here! it might produce weird results due to racing
+      
+      myFaces = _faces.map(x=>packFace(x,VTX)); // update the global myFaces object with the detected faces
+
+      // console.log(myFaces);
+      if (!myFaces.length){
+        // haven't found any faces
+        statusText = "Show some faces!"
+      }else{
+        // display the confidence, to 3 decimal places
+        statusText = "Confidence: "+ (Math.round(_faces[0].faceInViewConfidence*1000)/1000) + "Frame Rate: " + frameRate();
+      }
+      
+    })
+  }
+  
+  background(0, 100, 100);
+  
+  //debug
+  // push();
+  // scale(0.5); // downscale the webcam capture before drawing, so it doesn't take up too much screen sapce
+  // image(capture, 0, 0, capture.width, capture.height);
+  // noFill();
+  // stroke(255,0,0);
+  // drawFaces(myFaces); // draw my face skeleton
+  // pop();
+  
+  
+  // now draw all the other users' faces (& drawings) from the server
+  push()
+  
+  scale(3);
+  
+  translate(-100, -200)
+  drawFaces(myFaces);
+  pop();
+  
+  push();
+  fill(255,255,0);
+  text(statusText,2,200);
+  pop();
+}
+
+
+// draw a face object returned by facemesh
+function drawFaces(faces,filled){
+
+  for (var i = 0; i < faces.length; i++){
+    const keypoints = faces[i].scaledMesh;
+
+    for (var j = 0; j < keypoints.length; j++) {
+      const [x, y, z] = keypoints[j];
+      fill(255);
+      noStroke();
+      circle(x,y,2);
+    }
+  }
+}
+
+// reduces the number of keypoints to the desired set 
+function packFace(face,set){
+  var ret = {
+    scaledMesh:[],
+  }
+  for (var i = 0; i < set.length; i++){
+    var j = set[i];
+    ret.scaledMesh[i] = [
+      face.scaledMesh[j][0],
+      face.scaledMesh[j][1],
+      face.scaledMesh[j][2],
+    ]
+  }
+  return ret;
+}
+
+
+
 
 function recordVideo() {
   console.log("RECORDING")
@@ -92,17 +191,12 @@ function exportVideo(e) {
   downloadVideo();
 }
 
-// Load the MediaPipe facemesh model assets.
-facemesh.load().then(function(_model){
-  statusText = "Model loaded."
-  facemeshModel = _model;
-})
 
 function configSound(){
 
-    userStartAudio();
+  userStartAudio();
 
-      // create an audio in
+  // create an audio in
   mic = new p5.AudioIn();
 
   // prompts user to enable their browser mic
@@ -119,107 +213,4 @@ function configSound(){
   soundFile = new p5.SoundFile();
 
   userStartAudio();
-}
-
-function setup() {
-  configSound();
-  btn = document.querySelector('button');
-  btn.onclick = recordVideo;
-
-  createCanvas(1200, 720);
-  capture = createCapture(VIDEO);
-  
-  // capture = createVideo("test.mov");
-
-  // this is to make sure the capture is loaded before asking facemesh to take a look
-  // otherwise facemesh will be very unhappy
-  capture.elt.onloadeddata = function(){
-    console.log("video initialized");
-    videoDataLoaded = true;
-  }
-  
-  capture.hide();
-  background(0)
-}
-
-
-// draw a face object returned by facemesh
-function drawFaces(faces,filled){
-
-  for (var i = 0; i < faces.length; i++){
-    const keypoints = faces[i].scaledMesh;
-
-    for (var j = 0; j < keypoints.length; j++) {
-      const [x, y, z] = keypoints[j];
-      fill(255);
-      noStroke();
-      circle(x,y,1);
-    }
-  }
-}
-
-// reduces the number of keypoints to the desired set 
-function packFace(face,set){
-  var ret = {
-    scaledMesh:[],
-  }
-  for (var i = 0; i < set.length; i++){
-    var j = set[i];
-    ret.scaledMesh[i] = [
-      face.scaledMesh[j][0],
-      face.scaledMesh[j][1],
-      face.scaledMesh[j][2],
-    ]
-  }
-  return ret;
-}
-
-function draw() {
-  strokeJoin(ROUND); //otherwise super gnarly
-  
-  if (facemeshModel && videoDataLoaded){ // model and video both loaded, 
-    
-    facemeshModel.estimateFaces(capture.elt).then(function(_faces){
-      // we're faceling an async promise
-      // best to avoid drawing something here! it might produce weird results due to racing
-      
-      myFaces = _faces.map(x=>packFace(x,VTX)); // update the global myFaces object with the detected faces
-
-      // console.log(myFaces);
-      if (!myFaces.length){
-        // haven't found any faces
-        statusText = "Show some faces!"
-      }else{
-        // display the confidence, to 3 decimal places
-        statusText = "Confidence: "+ (Math.round(_faces[0].faceInViewConfidence*1000)/1000) + "Frame Rate: " + frameRate();
-      }
-      
-    })
-  }
-  
-  background(0, 0, 100);
-  
-  //debug
-  // push();
-  // scale(0.5); // downscale the webcam capture before drawing, so it doesn't take up too much screen sapce
-  // image(capture, 0, 0, capture.width, capture.height);
-  // noFill();
-  // stroke(255,0,0);
-  // drawFaces(myFaces); // draw my face skeleton
-  // pop();
-  
-  
-  // now draw all the other users' faces (& drawings) from the server
-  push()
-  
-  scale(3);
-  
-  translate(-100, -200)
-  drawFaces(myFaces);
-  pop();
-  
-  push();
-  fill(255,0,0);
-  text(statusText,2,200);
-  pop();
 }
